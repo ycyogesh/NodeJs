5144cf1ef66fe93758b18c8f65981e2d31ba5653
ycyogesh <ycyogesh183@gmail.com>
2022-08-16 12:44:49 +0530

    first io

--- my-first-io.js
+++ my-first-io.js
@@ -1,7 +1,17 @@
-const fs = require('fs')  
+let a = 45;
+console.log(a);
+const fs = require('fs') 
+// console.log(fs);
 
 let buf = fs.readFileSync(process.argv[2])
+console.log(buf);
+a = 55;
+console.log(a);
 
-const str = buf.toString().split("\n").length-1
 
-console.log(str);
+// const str = buf.toString().split("\n").length-1
+
+// console.log(str);
+
+
+
