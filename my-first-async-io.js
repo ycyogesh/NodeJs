facb6ba1bc506b3555db3f86329d9d24af1a620d
ycyogesh <ycyogesh183@gmail.com>
2022-08-16 13:27:52 +0530

    first async

--- my-first-async-io.js
+++ my-first-async-io.js
@@ -3,5 +3,6 @@ let fs = require('fs')
 
 fs.readFile(process.argv[2],function(err,data){
     data = data.toString().split("\n").length-1
-    console.log(data);   
+    console.log(data);  
+    console.log(err); 
 })
